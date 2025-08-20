1a46996d4f72fcf480fd61532819193dbfcc7707
manassetimoteo4 <manassetimoteo4@gmail.com>
2025-08-20 15:52:25 +0100

    Finish building the main app layout

--- src/app/shared/components/main.jsx
+++ src/app/shared/components/main.jsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 
 function Main() {
   return (
-    <main className="row-start-2 col-span-2 ">
+    <main className="row-start-2  col-span-2 ">
       <Outlet />
     </main>
   );
