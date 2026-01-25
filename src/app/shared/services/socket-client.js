bdc7ffc284ef480c143ef03f6735b450e8216de5
manassetimoteo04 <manassetimoteo4@gmail.com>
2026-01-25 19:17:35 +0100

    fix: enviroment key

--- src/app/shared/services/socket-client.js
+++ src/app/shared/services/socket-client.js
@@ -4,7 +4,7 @@ let socket = null;
 
 export function getSocket() {
   if (!socket) {
-    socket = io(import.meta.env.VITE_API_URL || "http://localhost:5000", {
+    socket = io(import.meta.env.VITE_SOCKET_URL, {
       withCredentials: true,
       transports: ["websocket"],
     });
